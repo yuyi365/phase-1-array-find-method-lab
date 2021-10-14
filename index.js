0d611e838c32b7a1d62f7cb3b236c95acde05c78
Yuyi <yuyi365@gmail.com>
2021-10-14 13:30:17 -0400

    commit tree 770075bf75e6e25e088cb360b39dde56fb600f04

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+/* Function Purpose:
+    1. Test if the array object is "W"
+    2. If it is W, return value of key
+    result is the key
 
+*/
+
+// const superbowlWin = (object) => {
+//     let winningResults = object.result === "W";
+
+//     if (winningResults) {
+//         return winningResults.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+const winningTest = (object) => {
+    return object.result === "W"
+}
+
+// let listOfRecords = record.find(superbowlWin);
+//     if (listOfRecords) {
+//         console.log(listOfRecords.year);
+//     } else{
+//         console.log(undefined);
+//     }
+
+const superbowlWin = (object) => {
+    let winningResults = object.find(winningTest);
+    
+    if (winningResults) {
+        return winningResults.year;
+    } else {
+        return undefined;
+    }
+} 
